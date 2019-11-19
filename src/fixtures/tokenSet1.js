32a81380d87db3dddc09f3d52415240e9016bcff
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-19 14:28:13 -0800

    commit fixtures

---
+++ src/fixtures/tokenSet1.js
@@ -0,0 +1,15 @@
+const tokenSet1 = [
+    {
+        name: 'body',
+        type: 'font-family',
+        usage: 'Use for body fonts',
+        value: '"Roboto Condensed", Arial, sans',
+    },
+    {
+        name: 'primary',
+        type: 'color',
+        usage: 'the primary branding color',
+        value: 'aliceblue',
+    },
+];
+export default tokenSet1;
