c4596f5a1db6e85696813c61b2ef6f36bdf91e4f
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-19 14:28:51 -0800

    save work

---
+++ src/Teikn.ts
@@ -0,0 +1,26 @@
+import { Generator, generators } from './Generators';
+import * as plugins from './Plugins';
+import { Token } from './Token';
+
+export class Teikn {
+  generators: Generator[];
+
+  plugins: plugins.Plugin[];
+
+  static generators = generators;
+
+  static plugins = plugins;
+
+  constructor(options: { generators: Generator[]; plugins: plugins.Plugin[] }) {
+    this.generators = options.generators;
+    this.plugins = options.plugins;
+  }
+
+  transform(tokens: Token[]) {
+    const map = new Map<Generator, string>();
+    this.generators.forEach(generator => {
+      map.set(generator, generator.generate(tokens, this.plugins));
+    });
+    console.log(map);
+  }
+}
