c12160cfe31ec162bb84e7a96f17c551ffb152b4
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-19 14:59:45 -0800

    updates for building

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,17 +1,24 @@
-const Teikn = require('..');
-
+const { Teikn } = require('..');
+console.log(Teikn);
+console.log(Teikn.generators);
+console.log(Teikn.plugins);
+console.log(Teikn.generators.SCSS);
 const tokens = [{ name: 'test', value: 'red', type: 'color', usage: 'TODO' }];
 
 const Writer = new Teikn({
   generators: [
-    Teikn.generators.SCSS,
-    Teikn.generators.SCSSVars,
-    Teikn.generators.SCSSCSSVars,
-    Teikn.generators.ESM,
-    Teikn.generators.TypeScript,
-    Teikn.generators.JSON,
+    new Teikn.generators.SCSS(),
+    new Teikn.generators.SCSSVars(),
+    // Teikn.generators.SCSSCSSVars(),
+    // Teikn.generators.ESM(),
+    new Teikn.generators.TypeScript(),
+    new Teikn.generators.Json(),
   ],
-  plugins: [...Teikn.defaultPlugins, MyOtherPlugin],
+  plugins: [
+    new Teikn.plugins.ColorTransformPlugin({ type: 'rgb' }),
+    new Teikn.plugins.PrefixTypePlugin(),
+    new Teikn.plugins.SCSSQuoteValuePlugin(),
+  ],
   outDir: './tokens',
 });
 
