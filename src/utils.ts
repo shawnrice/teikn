4d6fda9ae5a868efcc8f62453be292aa1768f41e
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-19 10:35:01 -0800

    move pad0 to upper scope

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,26 @@
+export const pad0 = (num: number | string) => {
+  if (typeof num === 'string') {
+    return num.length < 2 ? `0${num}` : num;
+  }
+
+  if (typeof num === 'number') {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  throw new TypeError('pad0 can only work with numbers and strings');
+};
+
 /**
  * Default date function that is used if another one is not passed
  */
 export const getDate = () => {
-  const pad0 = (num: number) => (num < 10 ? `0${num}` : num);
-
   const now = new Date();
   const date = now.toDateString();
-  const time = [now.getHours(), pad0(now.getMinutes()), pad0(now.getSeconds())].join(':');
+  const time = [
+    now.getHours(),
+    pad0(now.getMinutes()),
+    pad0(now.getSeconds()),
+  ].join(':');
   return `${date} ${time}`;
 };
 
