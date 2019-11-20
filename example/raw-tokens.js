9bc43d767bf4c9956e8955d90fc5e26ae466e1b6
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-20 01:35:32 -0800

    annotate example usage

--- example/raw-tokens.js
+++ example/raw-tokens.js
@@ -1,24 +1,4 @@
-const colors = [
-  {
-    name: 'primary',
-    value: 'steelblue',
-    type: 'color',
-    usage: 'Primary branding color',
-  },
-  {
-    name: 'error',
-    value: 'red',
-    type: 'color',
-    usage: 'Use for error states',
-  },
-  {
-    name: 'text-primary',
-    value: 'rgba(0, 0, 0, .95)',
-    type: 'color',
-    usage: 'Use for prominent text',
-  },
-];
-
+// Here are some font tokens that we'll use
 const fonts = [
   {
     name: 'body',
@@ -34,6 +14,28 @@ const fonts = [
   },
 ];
 
+// Here be the colors that we'll use
+const colors = [
+  {
+    name: 'primary',
+    value: 'steelblue',
+    usage: 'Primary branding color',
+  },
+  {
+    name: 'error',
+    value: 'red',
+    usage: 'Use for error states',
+  },
+  {
+    name: 'text-primary',
+    value: 'rgba(0, 0, 0, .95)',
+    usage: 'Use for prominent text',
+  },
+  // Since these are all the same type, we can just map the type into each token at the end
+].map(token => ({ ...token, type: 'color' }));
+
+// Since we defined the tokens in different arrays, we need to combine them
 const tokens = [...colors, ...fonts];
 
+// Don't forget to export
 module.exports = { tokens };
