5b5f6eb06a7364f3c30447cc3d13cc31d4fb9e15
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-20 00:55:12 -0800

    fix case sensitivity, maybe

--- src/generators/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Generator';
-export * from './JavaScript';
-export * from './Json';
-export * from './Scss';
-export * from './ScssVars';
-export * from './TypeScript';
--- src/generators/json.test.ts
+++
@@ -1,8 +0,0 @@
-import { default as tokenSet1 } from '../fixtures/tokenSet1';
-import Generator from './Json';
-
-describe('JSONGenerator tests', () => {
-  test('It generates the token set', () => {
-    expect(new Generator().generate(tokenSet1)).toMatchSnapshot();
-  });
-});
