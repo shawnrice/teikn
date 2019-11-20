aba610c6d0761a52bdf26d2d78f8d17bdbd9f056
Shawn Patrick Rice <rice@shawnrice.org>
2019-11-19 21:59:09 -0800

    loosen token type; remove old types

--- src/Token.ts
+++ src/Token.ts
@@ -1,27 +1,6 @@
-export type TokenTypes = 'color' | 'font' | 'font-family';
-
-export interface Token<Types extends TokenTypes = TokenTypes> {
+export interface Token {
   name: string;
   value: any;
   usage?: string;
-  type: Types;
-}
-
-export interface ColorToken extends Token<'color'> {}
-
-export interface FontToken extends Token<'font'> {}
-
-export interface FontFamilyToken extends Token<'font-family'> {}
-
-export enum ColorTransformPreference {
-  HSL = 'HSL',
-  HSLA = 'HSLA',
-  RGB = 'RGB',
-  RGBA = 'RGBA',
-  HEX = 'HEX',
-  HEX6 = 'HEX6',
-}
-
-export interface TokenTransformOptions {
-  color?: ColorTransformPreference;
+  type: string;
 }
