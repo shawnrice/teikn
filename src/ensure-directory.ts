4488f231c11f6015287ea5af485ae0b108d3b1b7
Shawn Patrick Rice <rice@shawnrice.org>
2020-07-08 13:40:07 -0700

    fix lint error

--- src/ensure-directory.ts
+++ src/ensure-directory.ts
@@ -1,25 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 
-export const ensureDirectory = (dirPath: string): Promise<boolean> =>
-  new Promise(async (res, rej) => {
-    const dir = path.resolve(dirPath);
-    try {
-      await fs.promises.access(dir);
-      res();
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        rej(err);
-      }
-
-      try {
-        await fs.promises.mkdir(dir, { mode: 0o755, recursive: true });
-        console.log(`Created ${dir}.`);
-        return res();
-      } catch (error) {
-        rej(error);
-      }
+export const ensureDirectory = async (dirPath: string): Promise<void> => {
+  const dir = path.resolve(dirPath);
+  try {
+    await fs.promises.access(dir);
+    return Promise.resolve();
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      return Promise.reject();
     }
-  });
+
+    try {
+      await fs.promises.mkdir(dir, { mode: 0o755, recursive: true });
+      console.log(`Created ${dir}.`);
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+};
 
 export default ensureDirectory;
