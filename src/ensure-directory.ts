cd20d02c27156af0568ec5fd46587685724963ed
Shawn Patrick Rice <rice@shawnrice.org>
2020-07-08 13:30:30 -0700

    rewrite to use fs.promises instead of mkdirp

--- src/ensure-directory.ts
+++ src/ensure-directory.ts
@@ -1,31 +1,23 @@
 import fs from 'fs';
-import mkdirp from 'mkdirp';
 import path from 'path';
 
 export const ensureDirectory = (dirPath: string): Promise<boolean> =>
-  new Promise((res, rej) => {
+  new Promise(async (res, rej) => {
     const dir = path.resolve(dirPath);
     try {
-      fs.accessSync(dir);
-      return res(true);
-    } catch (error) {
-      const { code } = error;
-      if (code === 'ENOENT') {
-        // The directory does not exist
-        mkdirp(dir, { mode: 0o755 & ~process.umask(0o755) })
-          .then(err => {
-            if (err) {
-              return rej(err);
-            }
+      await fs.promises.access(dir);
+      res();
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        rej(err);
+      }
 
-            console.log(`Created ${dir}.`);
-            return res(true);
-          })
-          .catch(error => {
-            rej(error);
-          });
-      } else {
-        return rej(error);
+      try {
+        await fs.promises.mkdir(dir, { mode: 0o755, recursive: true });
+        console.log(`Created ${dir}.`);
+        return res();
+      } catch (error) {
+        rej(error);
       }
     }
   });
